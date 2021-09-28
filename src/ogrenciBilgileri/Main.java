2b21126270be2d00783e51ca14163fd5e5b88d41
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 14:06:31 +0300

    Initial commit

---
+++ src/ogrenciBilgileri/Main.java
@@ -0,0 +1,28 @@
+package ogrenciBilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ogrenciBilgileri");
+		System.out.print("ÝSÝM");
+		System.out.print(" ");
+		System.out.print("Soyisim:");
+		System.out.println("emir akbasoglu");
+		System.out.print("Yaþ:");
+		System.out.println("15");
+		System.out.print("Doðum yeri:");
+		System.out.println("BOLU");
+		System.out.print("Okulu:");
+		System.out.println("Ýzzet baysal mesleki ve teknik anadolu lisesi");
+		
+		
+		
+	
+		
+		
+				
+
+	}
+
+}
